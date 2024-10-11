30ec9c9653af97301a8de9ccd8826eb1348e7121
RAJ <mdrajahmad686@gmail.com>
2024-10-11 11:06:00 +0600

    Design Complete 50%

---
+++ lib/login.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //       SET background Image start
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('asset/login.png'), fit: BoxFit.cover)),
+      //       SET background Image close
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 35, top: 130),
+              child: Text(
+                "WELCOME \nBACK",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                ),
+              ),
+            ),
+
+            //===========FOR TEXT FIELD
+            Container(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.5,
+                  left: 35,
+                  right: 35),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        fillColor: Colors.grey.shade100,
+                        filled: true,
+                        hintText: 'Email',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        )
+
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey.shade100,
+                      filled: true,
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+
+                      )
+                    ),
+                  )
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
